dc00fd3665c9b281b85944aaad3c0daf05ad028b
codelabuk <afzal232@gmail.com>
2026-04-12 00:37:35 +0530

    add scala jobs config set ups & docker file for downloadin dependent jars for spark-history

--- jobs/scala/scalasets-job/build.sbt
+++ jobs/scala/scalasets-job/build.sbt
@@ -1,7 +1,24 @@
+
+import scala.language.postfixOps
+
 name := "Word Count"
 organization := "org.codelabuk"
-version := "0.1"
+version := "0.2"
 
 scalaVersion := "2.12.18"
 
-libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.5.1"
+libraryDependencies ++= Seq("org.apache.spark" %% "spark-sql" % "3.5.1" % "provided",
+  "com.typesafe" % "config" % "1.4.2"
+)
+
+assembly / assemblyJarName := "word-abt.jar"
+
+assembly / assemblyMergeStrategy := {
+  case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+  case "application.conf"  => MergeStrategy.concat
+  case "reference.conf" => MergeStrategy.concat
+  case x =>
+    val old = (assembly / assemblyMergeStrategy).value
+    old(x)
+}
+
--- jobs/scala/scalasets-job/src/main/scala/org/codelabuk/MainSparkLabuk.scala
+++ jobs/scala/scalasets-job/src/main/scala/org/codelabuk/MainSparkLabuk.scala
@@ -1,15 +1,22 @@
 package org.codelabuk
 
+import com.typesafe.config.ConfigFactory
 import org.apache.spark.sql.SparkSession
 
 object MainSparkLabuk {
 
   def main(args: Array[String]): Unit = {
-    val spark = SparkSession.builder().appName("Sellers-City").getOrCreate()
+    val config = ConfigFactory.load()
+    val appName = config.getString("app.name")
+    val inputPath = config.getString("app.file.inputPath")
+    val spark = SparkSession.builder().appName(appName).getOrCreate()
+
     val df = spark.read.option("header","true")
-      .csv("s3a://data/csv/sellers/")
+      .csv(inputPath)
     df.printSchema()
     df.show(10)
+    val logicalRDDplan = df.queryExecution.analyzed
+    logicalRDDplan.printSchema()
   }
 
 }
